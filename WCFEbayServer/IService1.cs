80e27de5adb69874d648f79310306c90b3b41e10
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-05-17 21:20:23 +0200

    the server
    handle the info from the database and send it to the Client

---
+++ WCFEbayServer/IService1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFEbayServer
+{
+   // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+   [ServiceContract]
+   public interface IService1
+   {
+
+      [OperationContract]
+      [FaultContract(typeof(UnsuccessfullLoginFault))]
+      string login(string username, string password);
+
+      [OperationContract]
+      [FaultContract(typeof(ServiceData))]
+      ServiceData TestConnection(string strConnectionString);
+
+      [OperationContract]
+      string bye();
+      [OperationContract]
+      string Add(string name, string code, int price, string uid);
+
+      [OperationContract]
+      List<Product> List();
+      [OperationContract]
+      string Bid(string code, int price, string uid);
+      // TODO: Add your service operations here
+
+      //used functions 
+      [OperationContract]
+      Car GetCars(int id);
+
+      [OperationContract]
+      void SaveCars(Car employee);
+
+      [OperationContract]
+      void DeleteCars(int id);
+
+      [OperationContract]
+      List<Car> allCars();
+
+
+
+      [OperationContract]
+      void SaveUsers(users normalUser);
+
+      [OperationContract]
+      void DeleteUsers(int id);
+
+      [OperationContract]
+      users GetUsers(int id);
+
+      [OperationContract]
+      List<users> allUsers();
+   }
+   [DataContract]
+   public class ServiceData
+   {
+      [DataMember]
+      public bool Result
+      { get; set; }
+      [DataMember]
+      public string ErrorMessage
+      { get; set; }
+      [DataMember]
+      public string ErrorDetails
+      { get; set; }
+   }
+   [DataContract]
+   public class UnsuccessfullLoginFault
+   {
+      private string report;
+
+      public UnsuccessfullLoginFault(string message)
+      {
+         this.report = message;
+      }
+
+      [DataMember]
+      public string Message
+      {
+         get { return this.report; }
+         set { this.report = value; }
+      }
+   }
+
+   // Use a data contract as illustrated in the sample below to add composite types to service operations.
+   [DataContract]
+   public class Product
+   {
+
+      string name, code, user;
+      [DataMember]
+      public string User
+      {
+         get { return user; }
+         set { user = value; }
+      }
+      [DataMember]
+      public string Code
+      {
+         get { return code; }
+         set { code = value; }
+      }
+      [DataMember]
+      public string Name
+      {
+         get { return name; }
+         set
+         {
+            if (name.Length < 5)
+            {
+               throw new FaultException("name can't be less than 5 character");
+            }
+            else if (string.IsNullOrEmpty(name))
+            {
+               throw new FaultException("name can't be empty");
+            }
+            name = value;
+         }
+      }
+      int curr_price;
+      [DataMember]
+      public int Curr_price
+      {
+         get { return curr_price; }
+         set { curr_price = value; }
+      }
+
+      public Product()
+      {
+
+      }
+      public Product(string name, string code, int curr_price)
+      {
+         this.name = name; this.code = code; this.curr_price = curr_price;
+      }
+
+   }
+
+   public class Car
+   {
+      private int carID;
+      private string type, brand, license_plate;
+      [DataMember]
+      public int CarID
+      {
+         get { return carID; }
+         set { carID = value; }
+      }
+
+      [DataMember]
+      public string Type
+      {
+         get { return type; }
+         set { type = value; }
+      }
+      [DataMember]
+      public string Brand
+      {
+         get { return brand; }
+         set { brand = value; }
+      }
+      [DataMember]
+      public string License_plate
+      {
+         get { return license_plate; }
+         set { license_plate = value; }
+      }
+
+      public override string ToString()
+      {
+         StringBuilder sb = new StringBuilder();
+         sb.Append(String.Format("|{0,5}|{1,5}|{2,5}|{3,5}|",
+                                  this.CarID, this.Type, this.Brand, this.License_plate));
+         return sb.ToString();
+      }
+
+   }
+
+   public class rented_car
+   {
+      private int cardID, userID;
+      [DataMember]
+      public int CarID
+      {
+         get { return cardID; }
+         set { cardID = value; }
+      }
+      public int UserID
+      {
+         get { return userID; }
+         set { userID = value; }
+      }
+
+      public override string ToString()
+      {
+         StringBuilder sb = new StringBuilder();
+         sb.Append(String.Format("|{0,5}|{1,5}|", this.UserID, this.CarID));
+         return sb.ToString();
+      }
+   }
+
+   public class users
+   {
+      private int userID;
+      private string firstName, secondName;
+
+      [DataMember]
+      public int UserID
+      {
+         get { return userID; }
+         set { userID = value; }
+      }
+
+      [DataMember]
+      public string FirstName
+      {
+         get { return firstName; }
+         set { firstName = value; }
+      }
+      [DataMember]
+      public string SecondName
+      {
+         get { return secondName; }
+         set { secondName = value; }
+      }
+
+   }
+}
---
+++ WCFEbayServer/Service1.svc.cs
@@ -0,0 +1,415 @@
+﻿using Microsoft.Extensions.Hosting;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFEbayServer
+{
+   // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+   // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+   public class Service1 : IService1
+   {
+      public static List<Product> Products = new List<Product>();
+      static Dictionary<string, string> logged = new Dictionary<string, string>();
+      public static List<Car> AllCars = new List<Car>();
+      public static List<users> AllUsers = new List<users>();
+      public string user = null;
+      public string login(string user, string password)
+      {
+         try
+         {
+            this.user = user;
+            string id = Guid.NewGuid().ToString();
+            logged.Add(id, user);
+            return id;
+         }
+         catch (Exception ex)
+         {
+            throw new FaultException<UnsuccessfullLoginFault>(new UnsuccessfullLoginFault(ex.Message));
+         }
+
+      }
+      public string bye()
+      {
+         return "Bye-bye";
+      }
+      public string Add(string name, string code, int price, string id)
+      {
+         try
+         {
+            if (!logged.ContainsKey(id))
+            {
+               return "Log in";
+            }
+            else
+            {
+               lock (Products)
+               {
+                  int i = 0;
+                  for (i = 0; i < Products.Count && Products[i].Code != code; i++) ;
+                  if (i < Products.Count)
+                  {
+                     return "Exists";
+                  }
+                  else
+                  {
+                     Product temp = new Product(name, code, price);
+                     Products.Add(temp);
+                     return "OK";
+                  }
+               }
+            }
+         }
+         catch (Exception ex)
+         {
+            throw new FaultException<UnsuccessfullLoginFault>(new UnsuccessfullLoginFault(ex.Message));
+         }
+
+      }
+      public List<Product> List()
+      {
+         lock (Products)
+         {
+            return Products;
+         }
+      }
+
+      public List<Car> allCars()
+      {
+         lock (AllCars)
+         {
+            List<Car> AllCarsInside = new List<Car>();
+
+            string cs = @"server=localhost;User Id=root;Password=Ahmed22;Database=cartype";
+            using (MySqlConnection connection = new MySqlConnection(cs))
+            {
+               string query = "select * from car;";
+               MySqlCommand cmd = new MySqlCommand(query, connection);
+               cmd.CommandType = CommandType.Text;
+               try
+               {
+                  connection.Open();
+                  MySqlDataReader reader = cmd.ExecuteReader();
+                  {
+                     while (reader.Read())
+                     {
+                        Car normalCar = new Car();
+                        normalCar.Type = reader["Type"].ToString();
+                        normalCar.Brand = (reader["Brand"].ToString());
+                        normalCar.License_plate = (reader["license_plate"].ToString());
+                        normalCar.CarID = Convert.ToInt32(reader["carID"]) ;
+
+                        AllCarsInside.Add(normalCar);
+                     }
+                  }
+               }
+               finally
+               {
+                  connection.Close();
+               }
+               return AllCarsInside;
+            }
+
+         }
+      }
+
+
+      public List<users> allUsers()
+      {
+         lock (AllUsers)
+         {
+            List<users> AllUsersInside = new List<users>();
+
+            string cs = @"server=localhost;User Id=root;Password=Ahmed22;Database=cartype";
+            using (MySqlConnection connection = new MySqlConnection(cs))
+            {
+               string query = "select * from users;";
+               MySqlCommand cmd = new MySqlCommand(query, connection);
+               cmd.CommandType = CommandType.Text;
+               try
+               {
+                  connection.Open();
+                  MySqlDataReader reader = cmd.ExecuteReader();
+                  {
+                     while (reader.Read())
+                     {
+                        users normalUser = new users();
+
+                        normalUser.UserID = Convert.ToInt32(reader["UserID"]);
+                        normalUser.FirstName = (reader["firstName"].ToString());
+                        normalUser.SecondName = (reader["SecondName"].ToString());
+
+                        AllUsersInside.Add(normalUser);
+                     }
+                  }
+               }
+               finally
+               {
+                  connection.Close();
+               }
+               return AllUsersInside;
+            }
+
+         }
+      }
+
+      public string Bid(string code, int price, string id)
+      {
+         //checkPrice(price);
+         if (!logged.ContainsKey(id))
+         {
+            return "Log in";
+         }
+
+         else
+         {
+            Product t = null;
+            lock (Products)
+            {
+               int j = 0;
+               for (j = 0; j < Products.Count && Products[j].Code != code; j++) ;
+               if (j >= Products.Count)
+               {
+                  return "No such product";
+               }
+               else
+                   if (Products[j].Curr_price < price)
+               {
+                  Products[j].Curr_price = price;
+                  Products[j].User = user;
+                  return "OK";
+               }
+               else
+                  return "Low price";
+            }
+         }
+      }
+
+
+      #region to deal with DataBase
+
+      public void SaveCars(Car normalCar)
+      {
+         string cs = @"server=localhost;User Id=root;Password=Ahmed22;Database=cartype";//still
+         using (MySqlConnection connection = new MySqlConnection(cs))
+         {
+            MySqlCommand cmd = new MySqlCommand("SaveCars", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            MySqlParameter TypeParameter = new MySqlParameter //1
+            {
+               ParameterName = "@param_Type",
+               Value = normalCar.Type
+            };
+            cmd.Parameters.Add(TypeParameter);
+
+            MySqlParameter BrandParameter = new MySqlParameter //2
+            {
+               ParameterName = "@param_Brand",
+               Value = normalCar.Brand
+            };
+            cmd.Parameters.Add(BrandParameter);
+
+            MySqlParameter LicensePlateParameter = new MySqlParameter //3
+            {
+               ParameterName = "@param_License_plate",
+               Value = normalCar.License_plate
+            };
+            cmd.Parameters.Add(LicensePlateParameter);
+
+            MySqlParameter CaridParameter = new MySqlParameter //4
+            {
+               ParameterName = "@param_CarID",
+               Value = normalCar.CarID
+            };
+            cmd.Parameters.Add(CaridParameter);
+            try
+            {
+               connection.Open();
+               cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+               connection.Close();
+            }
+         }
+
+      }
+      public void DeleteCars(int id)
+      {
+         string cs = @"server=localhost;User Id=root;Password=Ahmed22;Database=cartype";
+         using (MySqlConnection connection = new MySqlConnection(cs))
+         {
+            MySqlCommand cmd = new MySqlCommand("DeleteCars", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@param_Id", MySqlDbType.Int32).Value = id;
+            try
+            {
+               connection.Open();
+               cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+               connection.Close();
+            }
+         }
+      }
+      public Car GetCars(int id)
+      {
+         Car normalCar = new Car();
+         string cs = @"server=localhost;User Id=root;Password=Ahmed22;Database=cartype";//still
+         using (MySqlConnection connection = new MySqlConnection(cs))
+         {
+            string query = "select * from car where carID=@param_ID;";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@param_ID", MySqlDbType.Int32).Value = id;
+            try
+            {
+               connection.Open();
+               MySqlDataReader reader = cmd.ExecuteReader();
+               while (reader.Read())
+               {
+                  normalCar.Type = reader["Type"].ToString();
+                  normalCar.Brand = (reader["Brand"].ToString());
+                  normalCar.License_plate = (reader["license_plate"].ToString());
+                  normalCar.CarID = Convert.ToInt32(reader["carID"]);
+               }
+            }
+            finally
+            {
+               connection.Close();
+            }
+         }
+         return normalCar;
+      }
+
+
+      public void SaveUsers(users normalUser)
+      {
+         string cs = @"server=localhost;User Id=root;Password=Ahmed22;Database=cartype";//still
+         using (MySqlConnection connection = new MySqlConnection(cs))
+         {
+            MySqlCommand cmd = new MySqlCommand("SaveUsers", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            MySqlParameter FirstNameParameter = new MySqlParameter //1
+            {
+               ParameterName = "@param_FirstName",
+               Value = normalUser.FirstName
+            };
+            cmd.Parameters.Add(FirstNameParameter);
+
+            MySqlParameter SecondNameParameter = new MySqlParameter //2
+            {
+               ParameterName = "@param_SecondName",
+               Value = normalUser.SecondName
+            };
+            cmd.Parameters.Add(SecondNameParameter);
+
+            MySqlParameter UserIDParameter = new MySqlParameter //3
+            {
+               ParameterName = "@param_UserID",
+               Value = normalUser.UserID
+            };
+            cmd.Parameters.Add(UserIDParameter);
+
+            try
+            {
+               connection.Open();
+               cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+               connection.Close();
+            }
+         }
+
+      }
+      public void DeleteUsers(int id)
+      {
+         string cs = @"server=localhost;User Id=root;Password=Ahmed22;Database=cartype";
+         using (MySqlConnection connection = new MySqlConnection(cs))
+         {
+            MySqlCommand cmd = new MySqlCommand("DeleteUsers", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@param_Id", MySqlDbType.Int32).Value = id;
+            try
+            {
+               connection.Open();
+               cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+               connection.Close();
+            }
+         }
+      }
+      public users GetUsers(int id)
+      {
+         users normalUsers = new users();
+         string cs = @"server=localhost;User Id=root;Password=Ahmed22;Database=cartype";//still
+         using (MySqlConnection connection = new MySqlConnection(cs))
+         {
+            string query = "select * from users where UserID=@param_ID;";
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add("@param_ID", MySqlDbType.Int32).Value = id;
+            try
+            {
+               connection.Open();
+               MySqlDataReader reader = cmd.ExecuteReader();
+               while (reader.Read())
+               {
+                  normalUsers.UserID = Convert.ToInt32(reader["UserID"]);
+                  normalUsers.SecondName = (reader["SecondName"].ToString());
+                  normalUsers.FirstName = (reader["firstName"].ToString());
+               }
+            }
+            finally
+            {
+               connection.Close();
+            }
+         }
+         return normalUsers;
+      }
+
+      #endregion
+
+
+
+      public ServiceData TestConnection(string strConnectionString)
+      {
+         ServiceData myServiceData = new ServiceData();
+         try
+         {
+            ServiceHost host = new ServiceHost(typeof(WCFEbayServer.Service1));
+            host.Open();
+            myServiceData.Result = true;
+            host.Close();
+            return myServiceData;
+         }
+         catch (SqlException sqlEx)
+         {
+            myServiceData.Result = false;
+            myServiceData.ErrorMessage = " I cannot connect" + "later?";
+            myServiceData.ErrorDetails = sqlEx.ToString();
+            throw new FaultException<ServiceData>(myServiceData, sqlEx.ToString());
+         }
+         catch (Exception ex)
+         {
+            myServiceData.Result = false;
+            myServiceData.ErrorMessage = "Unknown error";
+            myServiceData.ErrorDetails = ex.ToString(); throw new
+               FaultException<ServiceData>(myServiceData, ex.ToString());
+         }
+      }
+   }
+}
